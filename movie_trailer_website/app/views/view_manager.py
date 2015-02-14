1c3f78b67c79ebd1c8a6c2cace61b287ff521b7c
Rudi Jonker <rudi@customapp.co.za>
2015-02-14 16:31:38 +0200

    Getting to run via python shell

--- movie_trailer_website/app/views/view_manager.py
+++ movie_trailer_website/app/views/view_manager.py
@@ -3,8 +3,8 @@ import webbrowser
 
 #function to get the current directory of the view folder which holds the movie trailer html page which will be displayed in a web browser
 def get_movie_trailer_web_page():
-    dir = os.path.abspath(os.curdir)
-    view = os.path.join(dir,r"app\views\movie_trailers.html")
+    dir = os.path.abspath(os.curdir)    
+    view = os.path.join(dir + "\\",r"views\movie_trailers.html")
     return view
 
 #opens a passed in file in the web browser
